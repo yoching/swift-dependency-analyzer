02721f71a9316f6ea71aa75df144852b0f825737
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2022-12-19 11:27:16 +0100

    Add dependency example

--- Input/example.swift
+++ Input/example.swift
@@ -1,10 +1,14 @@
 class A {
-    let b: B
-    let b2: B
-    let c: C
-    let int: Int
+  let b: B
+  let b2: B
+  let c: C
+  func hoge() {
+
+  }
+  let int: Int
 }
 
 class B {
+  let a: A
 
 }
